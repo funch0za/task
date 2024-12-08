d64bcf6707d761e7325ac55d8d5a769df86f5530
Nadezhda Samartseva <nadya.sm101@yandex.ru>
2024-12-08 19:34:14 +0300

    build graph

--- задание/el-1-last/build_graph.cpp
+++ задание/el-1-last/build_graph.cpp
@@ -8,21 +8,21 @@
 using graph_t = std::unordered_map<std::string, std::vector<std::string>>;
 using visited_t = std::unordered_map<std::string, bool>;
 
-void dfs(visited_t &visited, std::string &current, graph_t &g,
+void dfs_build_graph(visited_t &visited, std::string &current, graph_t &g,
          std::string &parent) {
-  g[parent].push_back(current);
-
+ 
   if (visited[current]) {
     return;
   }
 
   visited[current] = true;
   if (current.find(".src") != std::string::npos) {
+    
     std::ifstream file("lists/" + current);
 
     std::string line;
     while (file >> line) {
-      dfs(visited, line, g, current);
+      dfs_build_graph(visited, line, g, current);
     }
   }
 }
@@ -32,19 +32,20 @@ int main() {
   visited_t visited;
 
   std::string list = "all.src", start = ".";
-  dfs(visited, list, graph, start);
-
   
-
+  dfs_build_graph(visited, list, graph, start);
   for (auto &p : graph) {
-    std::cout << p.first << " -> {";
-    for (std::size_t i = 0; i < p.second.size(); ++i) {
-      std::cout << p.second[i];
-
-      if (i != p.second.size() - 1) {
-        std::cout << ", ";
+    if (!p.second.empty()) {
+      std::cout << "\"" << p.first << "\" -> {";
+      for (std::size_t i = 0; i < p.second.size(); ++i) {
+        std::cout << "\"" << p.second[i] << "\"";
+  
+        if (i != p.second.size() - 1) {
+          std::cout << ", ";
+        }
       }
+      std::cout << "}\n";
     }
-    std::cout << "}\n";
   }
+
 }
